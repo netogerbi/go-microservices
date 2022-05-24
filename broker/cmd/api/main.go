89eed2a65ddb07f45b17aa28ccbb25aa3fbad59e
netogerbi2@hotmail.com <netogerbi@Reculoss-MacBook-Pro-2.local>
2022-05-24 16:09:28 -0300

    configured a basic http server

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
 }
