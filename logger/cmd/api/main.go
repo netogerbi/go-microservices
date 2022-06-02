4e91b7ef8cc90d4a264b69051ff9f151246316e2
netogerbi <netogerbi2@hotmail.com>
2022-06-02 10:55:19 -0300

    chore(logger): including and connecting to mongodb

--- logger/cmd/api/main.go
+++ logger/cmd/api/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongo://mongo:27017"
+	gRPCPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL).SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Default().Println("Error connection to MongoDB: ", err)
+		return nil, err
+	}
+	return c, nil
 }
