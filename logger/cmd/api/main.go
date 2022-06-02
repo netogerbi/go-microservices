eacd2e8284de19d2e281360d43cdf7fd7bf2221d
netogerbi <netogerbi2@hotmail.com>
2022-06-02 18:06:10 -0300

    feat(logger): added main configuration

--- logger/cmd/api/main.go
+++ logger/cmd/api/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"logger/data"
+	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -11,13 +15,14 @@ import (
 const (
 	webPort  = "80"
 	rpcPort  = "5001"
-	mongoURL = "mongo://mongo:27017"
+	mongoURL = "mongodb://mongo:27017"
 	gRPCPort = "50001"
 )
 
 var client *mongo.Client
 
 type Config struct {
+	Models data.Models
 }
 
 func main() {
@@ -26,6 +31,34 @@ func main() {
 		log.Panic(err)
 	}
 	client = mongoClient
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	app.serve()
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+
+	log.Println("Server started successfully!")
 }
 
 func connectToMongo() (*mongo.Client, error) {
@@ -39,5 +72,8 @@ func connectToMongo() (*mongo.Client, error) {
 		log.Default().Println("Error connection to MongoDB: ", err)
 		return nil, err
 	}
+
+	log.Println("Connected to MongoDB successfully!")
+
 	return c, nil
 }
