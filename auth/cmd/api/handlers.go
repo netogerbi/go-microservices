bc45f7a33a1373d9d1cce9f311dd1b3e9eb50f7b
netogerbi <netogerbi2@hotmail.com>
2022-06-03 16:27:15 -0300

    added a function to log authentications on logger service from auth service

--- auth/cmd/api/handlers.go
+++ auth/cmd/api/handlers.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -33,6 +36,12 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err = app.logRequest("Auth app", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		log.Printf("error trying to log authentication:  %v", err)
+		app.errorJSON(w, err, http.StatusBadRequest)
+	}
+
 	responsePayload := jsonResponse{
 		Error:   false,
 		Message: "Authentication successfull",
@@ -40,4 +49,29 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJSON(w, http.StatusOK, responsePayload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	j, _ := json.MarshalIndent(entry, "", "\t")
+	request, err := http.NewRequest(http.MethodPost, "http://logger/log", bytes.NewBuffer(j))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
