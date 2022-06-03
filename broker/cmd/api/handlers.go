346c9b7c7fa2b75029b2f42ea9d0a0bf6782828f
netogerbi <netogerbi2@hotmail.com>
2022-06-03 16:22:47 -0300

    feat(broker): add a handler to reach logger service using broker

--- broker/cmd/api/handlers.go
+++ broker/cmd/api/handlers.go
@@ -11,11 +11,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -39,9 +45,53 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
 		return
+	case "log":
+		app.logItem(w, requestPayload.Log)
+		return
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	log.Println("Logging...")
+
+	jsonData, err := json.MarshalIndent(entry, "", "\t")
+	if err != nil {
+		log.Println("Error trying to marshal log entry: ", err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	request, err := http.NewRequest(http.MethodPost, "http://logger/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println("Error trying create request to logger: ", err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println("Error on request to logger: ", err)
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusCreated {
+		log.Println("Logger error response: ", err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	responsePaylod := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJSON(w, response.StatusCode, responsePaylod)
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
