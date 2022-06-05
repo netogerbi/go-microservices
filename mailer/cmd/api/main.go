5da01a571d2aab86d8723da88c4fb53813d4e94f
netogerbi <netogerbi2@hotmail.com>
2022-06-05 11:56:13 -0300

    feat(mailer):added routes configuration and helpers

--- mailer/cmd/api/main.go
+++ mailer/cmd/api/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+type Config struct{}
+
+const (
+	webPort = "80"
+)
+
+func main() {
+	app := Config{}
+
+	if err := app.serve(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (app *Config) serve() error {
+	log.Printf("Starting mail service...")
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Println("Error starting server: ", err)
+		return err
+	}
+
+	return nil
 }
---
+++ mailer/cmd/api/routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"POST", "GET", "PUT", "OPTIONS", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CRFS-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
