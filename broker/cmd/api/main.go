9fcf5f2122df76b8c04edd48217df0853dd6b78d
netogerbi <netogerbi2@hotmail.com>
2022-06-22 11:15:31 -0300

    feat(broker): added rabbitmq connection on main

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const webPort = "80"
 
-type Config struct{}
+type Config struct {
+	RabbitMQ *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	mqconn, err := connect()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer mqconn.Close()
+
+	app := Config{
+		RabbitMQ: mqconn,
+	}
 
 	log.Printf("Starting broker service on port %s", webPort)
 
@@ -23,4 +37,34 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panic(err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = time.Second * 1
+	var conn *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			log.Println("Trying to connect to rabbitmq...")
+			counts++
+		} else {
+			log.Println("Successfully connected to RabbitMQ")
+			conn = c
+			break
+		}
+
+		if counts > 5 {
+			log.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2))
+		log.Printf("Backing off %d seconds until try again...", backOff)
+		time.Sleep(backOff)
+		continue
+	}
+
+	return conn, nil
 }
