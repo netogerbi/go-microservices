6f3cc853c5e1c454ca8236a504cd2653869d501f
netogerbi <netogerbi2@hotmail.com>
2022-07-14 09:14:16 -0300

    adjusted grpc protocol declarations and created the instalation of compiler and compilation in docker

---
+++ logger/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y protobuf-compiler
+
+export GO111MODULE=on
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+protoc --go_out=paths=source_relative:. ./logs/logs.proto --go-grpc_out=. ./logs/logs.proto
+
+# exec CMD from Dockerfile, i.e. "npm start"
+exec "$@"
