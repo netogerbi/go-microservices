109a2dadf63a5b4aa5094e5741ee1eecdd20ede9
netogerbi <netogerbi2@hotmail.com>
2022-08-31 21:21:11 -0300

    first GRPC service using autogenerated code from protobuf

--- logger/cmd/api/grpc.go
+++ logger/cmd/api/grpc.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"context"
+	"logger/data"
+	"logger/logs"
+)
+
+type LogServer struct {
+	logs.UnimplementedLogServiceServer
+	Models data.Models
+}
+
+func (l *LogServer) WriteLog(context context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogEntry()
+
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	if err := l.Models.LogEntry.Insert(logEntry); err != nil {
+		return &logs.LogResponse{Result: "Failed to write logs"}, err
+	}
+
+	return &logs.LogResponse{Result: "Success writing logs"}, nil
+}
